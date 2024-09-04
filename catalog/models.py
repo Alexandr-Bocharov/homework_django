430563b28b384c24658076b4b2d0435bbd5b53b0
tanjiro <counter23062001@mail.ru>
2024-09-04 18:04:04 +0300

    update models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,74 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Наименование категории",
+        help_text="Введите наименование категории"
+    )
+    description = models.TextField(
+        verbose_name='Описание категории',
+        help_text='Введите описание категории',
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Наименование товара",
+        help_text="Введите наименование товара"
+    )
+    description = models.TextField(
+        verbose_name='Описание товара',
+        help_text='Введите описание товара',
+        blank=True,
+        null=True
+    )
+    photo = models.ImageField(
+        verbose_name='Изображение',
+        help_text='Загрузите фото товара',
+        blank=True,
+        null=True
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        verbose_name = 'Категория',
+        help_text = 'Введите категорию товара',
+        blank=True,
+        null=True,
+        related_name='category'
+    )
+    price = models.IntegerField(
+        verbose_name = 'Цена',
+        help_text = 'Введите цену'
+    )
+    created_at = models.DateField(
+        verbose_name='Дата создания записи'
+    )
+    updated_at = models.DateField(
+        verbose_name='Дата последнего изменения записи'
+    )
+
+
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+
+    def __str__(self):
+        return self.name
+
+
+
