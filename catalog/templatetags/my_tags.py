d979f728c161dabb01abbeb744b8f8e4f44c2dbc
tanjiro <counter23062001@mail.ru>
2024-09-13 20:53:36 +0300

    add templatetags/my_tags

---
+++ catalog/templatetags/my_tags.py
@@ -0,0 +1,16 @@
+from django import template
+
+register = template.Library()
+
+@register.filter()
+def media_filter(path):
+    if path:
+        return f'/media/{path}'
+    return '#'
+
+
+@register.filter()
+def truncate_chars(text):
+    if len(text) > 100:
+        return text[:100]
+    return text
