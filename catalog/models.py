326f36056072b5a3ebab21e15706f2bcdc1a9883
tanjiro <counter23062001@mail.ru>
2024-10-02 18:02:30 +0300

    add Version

--- catalog/models.py
+++ catalog/models.py
@@ -27,17 +27,14 @@ class Product(models.Model):
     name = models.CharField(
         max_length=100,
         verbose_name="Наименование товара",
-        help_text="Введите наименование товара",
     )
     description = models.TextField(
         verbose_name="Описание товара",
-        help_text="Введите описание товара",
         blank=True,
         null=True,
     )
     photo = models.ImageField(
         verbose_name="Изображение",
-        help_text="Загрузите фото товара",
         blank=True,
         null=True,
     )
@@ -45,12 +42,11 @@ class Product(models.Model):
         Category,
         on_delete=models.SET_NULL,
         verbose_name="Категория",
-        help_text="Введите категорию товара",
         blank=True,
         null=True,
         related_name="category",
     )
-    price = models.IntegerField(verbose_name="Цена", help_text="Введите цену")
+    price = models.IntegerField(verbose_name="Цена")
     created_at = models.DateField(
         verbose_name="Дата создания записи", blank=True, null=True
     )
@@ -83,8 +79,34 @@ class Contacts(models.Model):
     )
 
     class Meta:
-        verbose_name = 'Контакт'
-        verbose_name_plural = 'Контакты'
+        verbose_name = "Контакт"
+        verbose_name_plural = "Контакты"
 
     def __str__(self):
         return self.inn
+
+
+class Version(models.Model):
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name="product",
+        verbose_name="продукт",
+    )
+    version_number = models.IntegerField(
+        verbose_name='номер версии',
+    )
+    version_name = models.CharField(
+        max_length=100,
+        verbose_name='название версии',
+    )
+    current_version_flag = models.BooleanField(
+        verbose_name='признак текущей версии',
+    )
+
+    def __str__(self):
+        return self.version_name
+
+    class Meta:
+        verbose_name = 'версия'
+        verbose_name_plural = 'версии'
