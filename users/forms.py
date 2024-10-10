c9d5a61bb2e051a3fefe510cf795022e03a8a125
tanjiro <counter23062001@mail.ru>
2024-10-10 21:50:43 +0300

    update

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,6 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm, PasswordResetForm, UserChangeForm
+from django.contrib.auth.views import PasswordResetConfirmView
+from django import forms
 
 from users.models import User
 
@@ -7,3 +9,19 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['email', 'password1', 'password2']
+
+
+class CustomPasswordResetForm(PasswordResetForm):
+    class Meta:
+        model = User
+        fields = ['email']
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name', 'avatar', 'phone_number', 'country')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['password'].widget = forms.HiddenInput()
