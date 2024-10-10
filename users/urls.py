e2ddff10e470f2c4d57c15991515cd4ea430df62
tanjiro <counter23062001@mail.ru>
2024-10-10 21:52:43 +0300

    update

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
-from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView, \
+    PasswordResetConfirmView, PasswordResetCompleteView
+from django.urls import path, reverse_lazy
 
 from users.apps import UsersConfig
-from users.views import RegisterView, email_verification
+from users.views import RegisterView, email_verification, CustomPasswordResetView, UserUpdateView
 
 app_name = UsersConfig.name
 
@@ -11,4 +12,11 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
     path('email-confirm/<str:token>/', email_verification, name='email-confirm'),
+    path('password-reset/',
+         CustomPasswordResetView.as_view(),
+         name='password_reset'),
+    path('password-reset/done/',
+         PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+         name='password_reset_done'),
+    path('profile/', UserUpdateView.as_view(), name='profile'),
 ]
