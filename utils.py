510952354607707d39132ff905d1041a9c528271
tanjiro <counter23062001@mail.ru>
2024-10-10 21:52:52 +0300

    update

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import json
 import random
+import secrets
+import string
 
 
 def write_data(data_new: dict):
@@ -17,3 +19,17 @@ def write_data(data_new: dict):
         json.dump(data, file, indent=4, ensure_ascii=False)
 
     print('Запись прошла успешно')
+
+
+def generation_password(symbols_num=10) -> str:
+    letters = string.ascii_letters
+    digits = string.digits
+    alphabet = letters + digits
+
+    new_password = ''
+
+    for _ in range(symbols_num):
+        new_password += secrets.choice(alphabet)
+
+    return new_password
+
